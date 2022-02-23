0f4a6c7163f1f1734d83581a628d4404c10ebe88
Estêvão <estevaopp05@gmail.com>
2022-02-23 10:53:20 -0300

    start maromba project

---
+++ app.py
@@ -0,0 +1,11 @@
+import os
+
+import sqlite
+from flask import Flask, redirect, render_template, request, session
+from flask_session import Session
+from tempfile import mkdtemp
+from werkzeug.security import check_password_hash, generate_password_hash
+
+app = Flask(__name__)
+
+
